13891323da6ddecdda7eaae0208f055b73608e5f
Chanakyha <vetrichanakyha2003@gmail.com>
2022-07-24 00:12:25 +0530

    Login Page is Done ✅

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,48 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
+import React, { useLayoutEffect } from "react";
 import useAuth from "../hooks/useAuth";
+import { useNavigation } from "@react-navigation/native";
+import tw from "tailwind-rn";
 
 const LoginScreen = () => {
-  const { signinWithGoogle } = useAuth();
+  const { signinWithGoogle, loading } = useAuth();
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, [navigation]);
 
   return (
-    <View>
-      <Text>Login Screen</Text>
-      <Button title="Login" onPress={signinWithGoogle} />
+    <View style={tw("flex-1")}>
+      <ImageBackground
+        resizeMode="cover"
+        style={tw("flex-1")}
+        source={{ uri: "https://tinder.com/static/tinder.png" }}
+      >
+        <TouchableOpacity
+          onPress={signinWithGoogle}
+          style={{
+            marginHorizontal: "25%",
+            position: "absolute",
+            bottom: "25%",
+            borderRadius: 15,
+            padding: "4%",
+            backgroundColor: "white",
+          }}
+        >
+          <Text style={tw("text-center font-semibold")}>
+            {loading ? "Loading..." : "Sign In & get swipping"}
+          </Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 };
