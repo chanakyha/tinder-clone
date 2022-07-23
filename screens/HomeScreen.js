741bc3f8c56a39119904a0aba2f6ba5342338263
Chanakyha <vetrichanakyha2003@gmail.com>
2022-07-23 21:08:08 +0530

    Installed TailwindCss in React Native

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import tw from "tailwind-rn";
+
+const HomeScreen = () => {
+  return (
+    <View style={tw("")}>
+      <Text style={tw("text-red-500")}>HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
