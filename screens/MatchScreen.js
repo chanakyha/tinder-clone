304185694795848bb4fb6bc4484d82c999a7c9a5
Chanakyha <vetrichanakyha2003@gmail.com>
2022-07-25 15:28:39 +0530

    Match Screen is Done ✅

--- screens/MatchScreen.js
+++ screens/MatchScreen.js
@@ -1,10 +1,41 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
-
+import { useNavigation, useRoute } from "@react-navigation/native";
+import tw from "tailwind-rn";
 const MatchScreen = () => {
+  const navigation = useNavigation();
+  const { params } = useRoute();
+
+  const { loggedInProfile, userSwiped } = params;
+
   return (
-    <View>
-      <Text>MatchScreen</Text>
+    <View style={[tw("h-full bg-red-500 pt-20"), { opacity: 0.89 }]}>
+      <View style={tw("justify-center px-10 pt-20")}>
+        <Image source={{ uri: "https://links.papareact.com/mg9" }} />
+      </View>
+
+      <Text style={tw("text-white text-center mt-5")}>
+        You and {userSwiped.displayName} have liked each other.
+      </Text>
+      <View style={tw("flex-row justify-evenly mt-5")}>
+        <Image
+          style={tw("h-32 w-32 rounded-full")}
+          source={{ uri: loggedInProfile.photoURL }}
+        />
+        <Image
+          style={tw("h-32 w-32 rounded-full")}
+          source={{ uri: userSwiped.photoURL }}
+        />
+      </View>
+      <TouchableOpacity
+        onPress={() => {
+          navigate.goBack();
+          navigation.navigate("Chat");
+        }}
+        style={tw("bg-white m-5 px-10 py-8 rounded-full mt-20")}
+      >
+        <Text style={tw("text-center")}>Send a Message</Text>
+      </TouchableOpacity>
     </View>
   );
 };
